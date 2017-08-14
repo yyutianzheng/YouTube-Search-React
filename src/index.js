f9bc08b566bb57d5263afdd8756d4d1d5c0c421d
yutian zheng <yutian.zheng@mail.mcgill.ca>
2017-08-13 21:45:23 -0400

    Easy React YouTube Search

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyANFBkP8p4NKNcMHHHKkexZoIb7r7EWqZE';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+  constructor(props){
+    super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+      };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    this.videoSearch("kotario oshio");
+}
+
+  videoSearch(term){
+     YTSearch({key: API_KEY, term: term}, (videos) => {
+     this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+    });
+  });
+
+  }
+  render() {
+    return (
+     <div>
+       <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+       <VideoDetail video={this.state.selectedVideo} />
+       <VideoList
+         onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+         videos={this.state.videos} />
+    </div>
+     );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
+
